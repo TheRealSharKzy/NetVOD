963d55b8a21c76751dca2746f7024a30853224d8
fulacse <1335857832@qq.com>
2022-11-10 09:36:40 +0100

    09:36

--- Action/ShowCatalogueAction.php
+++ Action/ShowCatalogueAction.php
@@ -11,15 +11,37 @@ class ShowCatalogueAction extends Action
 {
     private Serie $serie;
     public function execute(): string{
-        $page = "<form method='post'>
-<input type='text' name='find'><input type='submit' value='find'>
-</form>";
+        $page = "<form method='get'>
+<input type='text' ><input type='submit' value='find'>
+</form>
+tir par:
+<menu>
+<a href='?action=show-catalogue&tir=titre'><li>titre</li></a>
+<a href='?action=show-catalogue&tir=annee'><li>yeur</li></a>
+<a href='?action=show-catalogue&tir=date-ajout'><li>date make</li></a>
+<a href='?action=show-catalogue&tir=duree'><li>time</li></a>
+<a href='?action=show-catalogue&tir=nb-episode'><li>number of episode</li></a>
+<a href='?action=show-catalogue'><li>default</li></a>
+</menu>";
         $bdd = ConnectionFactory::makeConnection();
-        $res = $bdd->query("select * from serie");
-        $res->execute();
+        switch ($_GET['tir']){
+            case 'titre':
+                $sql="select * from serie order by titre";break;
+            case 'annee':
+                $sql="select * from serie order by annee";break;
+            case 'date-ajout':
+                $sql="select * from serie order by date_ajout";break;
+            case 'duree':
+                $sql="select serie.id,serie.titre,img,sum(duree) from serie left join episode on serie.id=serie_id group by serie.id,serie.titre,img order by sum(duree)";break;
+            case 'nb-episode':
+                $sql="select serie.id,serie.titre,img,count(episode.id) from serie left join episode on serie.id=serie_id group by serie.id,serie.titre,img order by count(episode.id)";break;
+            default:
+                $sql="select * from serie";break;
+        }
+        $res = $bdd->query($sql);
         while ($row = $res->fetch(\PDO::FETCH_ASSOC)) {
             $titre=$row['titre'];
-            if($this->http_method=='GET'||($this->http_method=='POST'&&preg_match("#".strtolower($_POST['find'])."#", strtolower($titre)))){//si le requet est GET ou le mot de chercher est correspondant
+            if(!isset($_GET['find'])&&preg_match("#".strtolower($_GET['find'])."#", strtolower($titre))){//si le mot de chercher est correspondant
                 $page .= "<div class='serie'>
                 <div class='serieDesc'>
                     <h2>" . $titre . "</h2>
